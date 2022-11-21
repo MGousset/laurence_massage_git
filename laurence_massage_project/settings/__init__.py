01fb300b9e9b78ce8bce803502b609155390ca3c
Jsnom <117653400+Jsnom@users.noreply.github.com>
2022-11-21 21:23:45 +0100

    Bug ip corriger slash
    proute

--- laurence_massage_project/settings/__init__.py
+++ laurence_massage_project/settings/__init__.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-pie7u5$$4&dol8gg%3#kxiaobj)*&i7w=opxd96#afkqo3g(s_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1/']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
